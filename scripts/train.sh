0486cd6b6a025a661e983d5aa54893bf408a78bd
Jan Ackermann <ackjan@stanford.edu>
2025-04-26 07:41:57 -0700

    Remove debug path from train script

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,7 +1,4 @@
-export  CUDA_VISIBLE_DEVICES=5,7,8 
-export PYTHONPATH=/home/w4756677/garment/AIpparel-Code 
-
-torchrun --standalone --nnodes=1 --nproc_per_node=3 scripts/run.py \
+torchrun --standalone --nnodes=1 --nproc_per_node=4 scripts/run.py \
  experiment.project_name=AIpparel \
  experiment.run_name=train \
  evaluate=False \
